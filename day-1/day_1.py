eaee32ee8c064111d9964296a4d0600dc3176a3d
antymijaljevic <antymijaljevic@gmail.com>
2023-01-22 22:12:04 +0100

    day 1

---
+++ day-1/day_1.py
@@ -0,0 +1,44 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+
+# using single quotes
+print("print('what to print')")
+
+# a new line
+print("Hello World\nHello World")
+
+# string concatenation
+print("Hello " + "Angela")
+print("Hello" + " " + "Angela")
+print("Hello", "Angela")
+
+#Fix the code below 👇
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# input build-in function
+print("Hello " + input("What is your name?") + "!")
+
+
+name = input("What is your name? ")
+print(len(name))
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
