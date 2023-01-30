2fc68e8b34239e9f6f71f930e392635a7663ef08
antymijaljevic <antymijaljevic@gmail.com>
2023-01-30 21:41:43 +0100

    go

---
+++ day-6-functions/day6.py
@@ -0,0 +1,112 @@
+# https://docs.python.org/3/library/functions.html
+
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+# # create function
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# # call function
+# my_function()
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+    
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+
+# # draw square
+# turn_left()
+# move()
+# move()
+# turn_right()
+# move()
+# move()
+# turn_right()
+# move()
+# move()
+# turn_right()
+# move()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def full_turn():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for _ in range(6):
+#     full_turn()
+
+# # while loop
+# num_of_hurdles = 6
+
+# while num_of_hurdles > 0:
+#     jump()
+#     num_of_hurdles -= 1
+#     print(num_of_hurdles)
+
+# # 1
+# while True:
+#     jump()
+#     if at_goal():
+#         break
+# # 2    
+# while at_goal() != True:
+#     jump()
+
+# # 3    
+# while not at_goal():
+#     jump()
+
+
+# while not at_goal():
+#     if wall_in_front():
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()
+#     else:
+#         move()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    if right_is_clear():
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    elif front_is_clear():
+        turn_right()
+    else:
+        move()
