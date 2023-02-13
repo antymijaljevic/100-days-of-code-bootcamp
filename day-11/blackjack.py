ae367d7fd68899e4cebc168b71d357bb7588c0d0
antymijaljevic <antymijaljevic@gmail.com>
2023-02-13 21:37:04 +0100

    day 11 done

--- day-11/blackjack.py
+++ day-11/blackjack.py
@@ -84,7 +84,7 @@ def game_monitor(cards, table_money):
           f"REMAINING CARDS = {len(cards)}\n" + 
           f"PLAYER BANK = {player_bank}\n" + 
           f"HOUSE BANK = {dealer_bank}\n" + 
-          f"BET = {table_money}")
+          f"PAYOUT = {table_money}")
     
 def deal(deck, num, existing_card = False):
     """
@@ -168,8 +168,8 @@ def money_flow(money, action):
         dealer_bank += money
         print("Dealer wins!")
     else:
-        dealer_bank += (money / 2)
-        player_bank += (money / 2)
+        dealer_bank += int(money / 2)
+        player_bank += int(money / 2)
         print("Draw!")
     
 def main():
@@ -184,11 +184,11 @@ def main():
     print(f"TOTAL CARD NUMBER = {len(shuffled_cards)}, DECK NUMBER = {deck_count}")
 
     """ play the game until one of player goes broke """
-    while not player_bank == 0:
+    while not player_bank <= 0:
         """ ask a player for a chip amount and reject incorrect input """    
         while True:
             print(f"\nPLAYER CHIP STACK = {player_bank}")
-            player_chip = input("What is your chip choice? '1', '5', '25', '50', '100', '500', '1000', 'YOLO'?  > ").lower()
+            player_chip = input(f"What is your chip choice? {chips}?  > ").lower()
             if player_chip in chips:
                 money_on_table = set_bet(player_chip)
                 break
@@ -208,24 +208,29 @@ def main():
         game_monitor(cards = remaining_cards, table_money = money_on_table)   
         
         """ double X2 logic """
-        if player_chip != "yolo":
-            if player_bank >= int(player_chip):
-                while True:
-                    double = input("\nDouble X2? 'yes' or 'enter' to continue: ").lower()
-                    if double == "yes":
-                        money_on_table = set_bet(player_chip, money_on_table)
-                        game_monitor(cards = remaining_cards, table_money = money_on_table)
-                        break
-                    elif not double:
-                        break
-                    else:
-                        print("Wrong input ...")
+        if not player_score == 21:
+            if player_chip != "yolo":
+                if player_bank >= int(player_chip):
+                    while True:
+                        double = input("\nDouble X2? 'yes' or 'enter' to continue: ").lower()
+                        if double == "yes":
+                            money_on_table = set_bet(player_chip, money_on_table)
+                            game_monitor(cards = remaining_cards, table_money = money_on_table)
+                            break
+                        elif not double:
+                            break
+                        else:
+                            print("Wrong input ...")
                 
         """ player play """
         dealer_play = True
-        
         while True:
+            """ player got Blackjack without move """
             print("\n#### Player's turn ####")
+            if player_score == 21:
+                print("**** Player got BlackJack ! ****")
+                break
+            
             hit_stand = input("'Hit' or 'Stand'? Which one is it?: ").lower()
             if hit_stand == "hit":
                 player_cards, remaining_cards = deal(deck = remaining_cards, num = 1, existing_card = player_cards)
@@ -295,43 +300,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
-
-
-
-
-
-
-
-"""
-Blackjack is a card game played between a dealer and one or more players. 
-The objective of the game is to have a hand value of 21 or as close to 21 as 
-possible, without going over.
-
-Here's a rundown of the basic rules:
-
-The game is played with one or more decks of standard playing cards.
-
-The cards are ranked as follows: Ace can be worth either 1 or 11, face cards 
-(King, Queen, Jack) are worth 10, and all other cards are worth their face value.
-
-The game begins with each player receiving two cards and the dealer receiving one
-card face up and one card face down.
-
-The player can choose to "hit" (take another card) or "stand" (keep their current hand). 
-The player continues to hit until they either have a hand value of 21 or over, or they choose to stand.
-
-If the player goes over 21, they "bust" and lose the game.
-
-After the player has finished their turn, the dealer reveals their face-down card and hits or stands according 
-to a set of rules. In most versions of Blackjack, the dealer must hit until their hand value is at least 17.
-
-If the dealer busts, all remaining players win.
-
-If neither the player nor the dealer busts, the hand with the highest value less than or equal to 21 wins.
-
-A "Blackjack" is a hand that consists of an Ace and any 10-point card (10, Jack, Queen, King). 
-Blackjacks pay out at 3:2 odds, unless the dealer also has a Blackjack, in which case the game is a 
-"push" (tie) and the player's bet is returned.
-
-"""
