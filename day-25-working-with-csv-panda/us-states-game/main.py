990f7c090aac1765699dd9bd64d45afb1001f69d
antymijaljevic <antymijaljevic@gmail.com>
2023-03-20 22:08:32 +0100

    moze

--- day-25-working-with-csv-panda/us-states-game/main.py
+++ day-25-working-with-csv-panda/us-states-game/main.py
@@ -1,7 +1,41 @@
 from turtle import Turtle, Screen
+from pandas import read_csv
 
+""" setup screen """
 screen = Screen()
 screen.title("U.S States Game")
+# screen.setup(width=700, height=500)
+image = "blank_states_img.gif"
+screen.addshape(image)
 
+""" map """
+turtle = Turtle()
+turtle.shape(image)
+
+""" save correct guesses """
+correct_guesses = []
+
+""" csv """
+dataframe = read_csv("50_states.csv")
+all_states = dataframe.state.to_list()
+
+
+while len(correct_guesses) < len(all_states):
+    """ asking user """
+    answer_state = screen.textinput(title=f"{len(correct_guesses)}/{len(all_states)} States Correct", prompt="What's another state's name?").title()
+
+    """ to exit game """
+    if answer_state == "Exit":
+        break
+
+    """ put state name into belonging state area"""
+    if answer_state in all_states:
+        new_turtle = Turtle()
+        new_turtle.hideturtle()
+        turtle.penup()
+        state_data = dataframe[dataframe.state == answer_state]
+        turtle.goto(int(state_data.x), int(state_data.y))
+        turtle.write(state_data.state.item())
+        correct_guesses.append(state_data.state.item())
 
 screen.exitonclick()
